ef681ad3a4875d7d650da0f254d86ddf1af36732
yaespa <yaya06938@gmail.com>
2024-08-20 03:15:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('mainImage').onclick = function() {
+
+    document.getElementById('additionalImages').classList.toggle('hidden');
+    
+    document.getElementById('clickMessage').classList.add('hidden');
+}
